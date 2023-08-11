efca974cb2a0f4ce512a537183854982982b70ea
Yusuf <88597615+TiggerBOW@users.noreply.github.com>
2023-08-11 17:40:51 +0300

    Add files via upload

---
+++ LevelsPanelManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelsPanelManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _levelBtnPrefab;
+    [SerializeField] private Transform _scrolWiewTransform;
+
+    private int _levelCount;
+
+    private void Start()
+    {
+        _levelCount = SceneManager.sceneCountInBuildSettings - 1;
+
+        InitializeLevelButtons();
+    }
+    private void InitializeLevelButtons()
+    {
+        for (int i = 0;i < _levelCount; i++)
+        {
+            int levelIndex = i + 1;
+            GameObject btn = Instantiate(_levelBtnPrefab, _scrolWiewTransform.position, Quaternion.identity);
+            btn.transform.parent = _scrolWiewTransform;
+            btn.GetComponentInChildren<Text>().text = "" + levelIndex;
+            Button btnComponent = btn.GetComponent<Button>();
+            btnComponent.onClick.AddListener(()=> LevelButtonTrigger(levelIndex));
+        }
+    }
+    private void LevelButtonTrigger(int level)
+    {
+        LevelManager.Instance.LoadLevelWithCoroutine(level);
+    }
+}
